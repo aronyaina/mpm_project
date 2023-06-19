d40c413981395b3d2d07197d59652f8eac569b64
root <ravaronyaina@gmail.com>
2023-06-19 16:32:13 +0300

    previous task fonctionnel and next task fonctionnel

--- src/services/taskService.py
+++ src/services/taskService.py
@@ -13,9 +13,10 @@ class TaskService:
             begin_task = Task.query.filter_by(name="Begin Task", project_id=self.project_id).first()
             self.task.previous_tasks.extend([begin_task])
             self.task.early_date = self.task.duration + begin_task.early_date
+            self.next_tasks = [task.to_json() for task in self.task.next_tasks]
         else:
             for previous_task in self.task.previous_tasks:
-                if self.is_directly_linked_to_prev_task(previous_task):
+                if self.is_directly_linked_to(previous_task):
                     self.previous_tasks.append(previous_task)
 
             self.task.previous_tasks = self.previous_tasks
@@ -36,11 +37,6 @@ class TaskService:
             end_task.late_date = end_task.early_date
             self.task.late_date = end_task.late_date - self.task.duration
         else:
-            # n_task = []
-            # for next_task in self.next_tasks:
-            #     if self.is_directly_linked_to_next_task(next_task):
-            #         n_task.append(next_task)
-            # self.task.next_tasks = n_task
             for next_task in self.next_tasks:
                 next_task_late_date = next_task["late_date"]
                 if next_task_late_date <= self.task.late_date:
@@ -56,7 +52,7 @@ class TaskService:
             self.task.is_critic = True
             return self.task.is_critic
 
-    def is_directly_linked_to_prev_task(self, linked_task):
+    def is_directly_linked_to(self, linked_task):
         visited = set()
         stack = list(self.task.previous_tasks)
 
@@ -66,20 +62,13 @@ class TaskService:
 
             if current_task == linked_task:
                 return True
-
+            if current_task.name == "End Task":
+                continue
             for prev_task in current_task.previous_tasks:
                 if prev_task not in visited:
                     stack.append(prev_task)
 
         return False
-
-    # def is_directly_linked_to_next_task(self, next_task):
-    #     self.next_tasks = [task.to_json() for task in self.task.next_tasks]
-    #     print(f"next task : {self.next_tasks}")
-    #     for nt in self.next_tasks:
-    #         if next_task in nt["previous_tasks"]:
-    #             return True
-    #     return False
 
     def call_all(self):
         self.get_early_date()
@@ -97,9 +86,20 @@ def synchronize_all_task(project_id):
         service = TaskService(task, project_id)
         if task.name == "Begin Task":
             begin_task.next_tasks = [task for task in begin_task.next_tasks if task.id != end_task_id]
+            next_t = [task for task in begin_task.next_tasks]
+            for t in next_t:
+                next_service = TaskService(t, project_id)
+                if next_service.is_directly_linked_to(end_task_id):
+                    continue
+                else:
+                    t.next_tasks = [task for task in t.next_tasks if task.name != "End Task"]
 
         elif task.name == "End Task":
-            end_task.previous_tasks = [task for task in end_task.previous_tasks if task.name != "End Task"]
+            previous_tasks = []
+            for _previous_task in task.previous_tasks:
+                if service.is_directly_linked_to(_previous_task):
+                    previous_tasks.append(_previous_task)
+            end_task.previous_tasks = previous_tasks
             if not end_task.previous_tasks:
                 end_task.early_date = 0
                 end_task.late_date = 0
